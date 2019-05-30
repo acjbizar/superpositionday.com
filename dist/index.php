288da63755ed63d8fc698a07c6c38a50ab5e51e0
acj <gitlab@acjs.net>
2019-05-30 22:51:05 +0200

    Current code (circa 2015).

---
+++ dist/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html class=myriad dir=ltr itemscope itemtype=https://schema.org/Event lang=en>
+<head>
+<meta charset=utf-8>
+<meta name=apple-mobile-web-app-capable content=yes>
+<meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
+<meta name=apple-mobile-web-app-title content=Superposition>
+<meta name=application-name content=Superposition>
+<meta name=description content="Today is ([in]ternational) Superposition Day, but also not (and everything in between, whatever that means). Science!">
+<meta name="theme-color" content="#FF0000">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@superpositional">
+<meta name="twitter:creator" content="@ACJ">
+<meta name="twitter:title" content="Superposition Day">
+<meta name="twitter:description" content="Today is ([in]ternational) Superposition Day, but also not (and everything in between, whatever that means). Science!">
+<meta name="twitter:image" content="https://superpositionday.com/apple-touch-icon-precomposed.png">
+<meta name=viewport content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
+<meta property="fb:admins" content="509248955">
+<meta property="og:description" content="Today is ([in]ternational) Superposition Day, but also not (and everything in between, whatever that means). Science!">
+<meta property="og:image" content="https://superpositionday.com/apple-touch-icon-precomposed.png">
+<meta property="og:image:height" content="768">
+<meta property="og:image:width" content="768">
+<meta property="og:title" content="Superposition Day">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://superpositionday.com/">
+<title itemprop=name>Superposition Day</title>
+<link rel=apple-touch-icon-precomposed href=apple-touch-icon-precomposed.png>
+<link rel=author href=https://alexanderchristiaanjacob.com/>
+<link rel=canonical href=https://superpositionday.com/>
+<style>
+*{border:0;box-sizing:border-box;font-family:inherit;font-size:1em;margin:0;outline:0;padding:0;position:relative;text-decoration:none}
+
+body{height:100%}
+html{font:normal 100%/1.5 sans-serif,serif;height:100%;text-rendering:optimizeLegibility}
+main{box-sizing:border-box;display:block;height:100%;margin:0 auto;padding:3em 0 3em 1.5em;max-width:48em}
+
+.and{font-size:1.5em;left:19em;position:absolute;top:11em}
+.but{font-size:3em;left:9.5em;position:absolute;top:4em}
+.memoriam{color:red;text-shadow:none;text-shadow:0 0 .125em rgba(255,0,0,.75);text-transform:none}
+.myriad{color:rgb(34, 105, 202);text-shadow:.03125em .03125em .03125em rgba(34, 105, 202,.5);text-transform:uppercase}
+.n{font-size:4.5em;line-height:1;text-indent:1.3333em}
+.no{font-size:1.5em;line-height:0}
+.q{font-size:2em;line-height:1.5}
+.qed{bottom:1em;position:fixed;right:1.5em}
+.qed a{color:rgba(2,251,2,.5)}
+/*.qm{display:inline-block}*/
+.science{left:13.5em;position:absolute;top:27em}
+.whatever{left:37.5em;position:absolute;top:19.5em}
+.yes{font-size:12em;line-height:1}
+
+
+@-moz-keyframes quanta {
+  0%,45%   { background-color: #fff; }
+  55%,100% { background-color: #000; }
+}
+@-ms-keyframes quanta {
+  0%,45%   { background-color: #fff; }
+  55%,100% { background-color: #000; }
+}
+@-o-keyframes quanta {
+  0%,45%   { background-color: #fff; }
+  55%,100% { background-color: #000; }
+}
+@-webkit-keyframes quanta {
+  0%,45%   { background-color: #fff; }
+  55%,100% { background-color: #000; }
+}
+@keyframes quanta {
+  0%,45%   { background-color: #fff; }
+  55%,100% { background-color: #000; }
+}
+
+html{
+   -moz-animation: quanta 4s infinite alternate;
+   -ms-animation: quanta 4s infinite alternate;
+   -o-animation: quanta 4s infinite alternate;
+   -webkit-animation: quanta 4s infinite alternate;
+   animation: quanta 4s infinite alternate;
+}
+
+/* https://daneden.github.io/animate.css/ */
+
+
+@media(max-width:48em)
+{
+	body{font-size:75%}
+}
+
+@media(max-width:36em)
+{
+	body{font-size:50%}
+}
+
+</style>
+<script src="https://use.typekit.net/kci1tgm.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6227584-59', 'auto');
+  ga('send', 'pageview');
+  
+</script>
+</head>
+<body id=top>
+<main id=content>
+
+<div>
+<div class="memoriam q">Is Today, <time class=myriad datetime=<?php echo date('Y-m-d'); ?>><?php echo strftime('%B ') , idate('d') , date(', Y'); ?></time></div>
+<div class="myriad n">Superposition Day<span class="qm wobble">?</span></div>
+</div>
+
+<div class="memoriam yes">Yes!</div>
+<div class="but">but also <span class="memoriam no">no</span></div>
+<div class="and">and <span class="memoriam">—everything—</span> in between</div>
+<div class="whatever">whatever that means&hellip;</div>
+
+<div class="science"><em>Science!</em></div>
+
+<div class=qed><a href=https://deidee.com/ lang=nl>■</a></div>
+
+</main>
+</body>
+</html>
